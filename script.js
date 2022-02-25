4933cc0584081bf867cffbb1e80748145dc464ba
LukeRuozzi <luca.ruozzi@gmail.com>
2022-02-25 17:52:07 +0100

    3 files modified, 1 added, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+const email = document.getElementById('email');
+const fieldWrapper = document.getElementsByClassName('field')[0];
+
+email.addEventListener('keyup', (e) => {
+  const text = e.target.value;
+  const valid = text === 'pippo';
+
+  console.log(text, valid)
+
+  if (valid) {
+    fieldWrapper.classList = 'field field-valid';
+  } else {
+    fieldWrapper.classList = 'field';
+  }
+
+});
