b48691f3c6cef8bd0e246dde342ffaa1263e2506
LukeRuozzi <luca.ruozzi@gmail.com>
2022-02-25 18:11:00 +0100

    3 files modified

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const fieldWrapper = document.getElementsByClassName('field')[0];
 
 email.addEventListener('keyup', (e) => {
   const text = e.target.value;
-  const valid = text === 'pippo';
 
-  console.log(text, valid)
+  const emailPattern = new RegExp(/\S+@\S+\.[a-z]{2,3}/gm);
+  const valid = emailPattern.test(text);
+
+  //console.log(text, valid)
 
   if (valid) {
     fieldWrapper.classList = 'field field-valid';
